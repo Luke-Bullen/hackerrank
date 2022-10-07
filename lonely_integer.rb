0a84c7a2a0a40150e1f94edcf4bb2dba402fc2c7
Luke Bullen <lukeabullen@gmail.com>
2022-10-07 15:40:31 +0100

    lonely integer challenge

---
+++ lonely_integer.rb
@@ -0,0 +1,38 @@
+# Instructions
+
+# Given an array of integers, where all elements but one occur twice, find the unique element.
+
+# Example
+
+# The unique element is .
+
+# Function Description
+
+# Complete the lonelyinteger function in the editor below.
+
+# lonelyinteger has the following parameter(s):
+
+# int a[n]: an array of integers
+# Returns
+
+# int: the element that occurs only once
+# Input Format
+
+# The first line contains a single integer, , the number of integers in the array.
+# The second line contains  space-separated integers that describe the values in .
+
+# Constraints
+
+# It is guaranteed that  is an odd number and that there is one unique element.
+# , where .
+
+
+# My Solution
+
+def lonelyinteger(a)
+  a.each do |x|
+      if a.count(x) == 1
+          return x
+      end
+  end
+end
